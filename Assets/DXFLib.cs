6c4513baac026016b85bce226a14b69b6c924d13
Andres Felipe Mendez <fmendez1212@gmail.com>
2016-11-14 12:01:05 -0500

    init

---
+++ Assets/DXFLib.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+public class DXFLib : MonoBehaviour
+{
+    [DllImport("ASimplePlugin", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void OpenFile(string path);
+
+    [DllImport("ASimplePlugin")]
+    private static extern IntPtr Path();
+    
+    [DllImport("ASimplePlugin")]
+    private static extern IntPtr GetStatus();
+
+
+    void Start ()
+    {
+        OpenFile(Application.dataPath);
+        Debug.Log(Marshal.PtrToStringAnsi(Path()));
+        Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+    }
+}
