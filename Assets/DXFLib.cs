ea714f9226cb7a4f37239c1c9eaa69532c16e1db
Andres Felipe Mendez <fmendez1212@gmail.com>
2016-11-14 18:42:58 -0500

    send file

--- Assets/DXFLib.cs
+++ Assets/DXFLib.cs
@@ -13,11 +13,44 @@ public class DXFLib : MonoBehaviour
     
     [DllImport("ASimplePlugin")]
     private static extern IntPtr GetStatus();
+
+    [DllImport("ASimplePlugin")]
+    private static extern int Hatches();
     
+    [DllImport("ASimplePlugin")]
+    private static extern IntPtr Array();
+
     void Start ()
     {
-        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\L.dxf");
+        float[] result = new float[5];
+        Marshal.Copy((IntPtr)Array(), result, 0, 5);
+        Debug.Log(result.Length);
+        Debug.Log(result[0]);
+
+
+        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\2000.dxf");
         Debug.Log(Marshal.PtrToStringAnsi(Path()));
         Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+        Debug.Log(Hatches());
+
+        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\2004.dxf");
+        Debug.Log(Marshal.PtrToStringAnsi(Path()));
+        Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+        Debug.Log(Hatches());
+
+        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\2007.dxf");
+        Debug.Log(Marshal.PtrToStringAnsi(Path()));
+        Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+        Debug.Log(Hatches());
+
+        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\2010.dxf");
+        Debug.Log(Marshal.PtrToStringAnsi(Path()));
+        Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+        Debug.Log(Hatches());
+
+        OpenFile(@"C:\Users\Len\Desktop\DXF\Assets\Resources\2013.dxf");
+        Debug.Log(Marshal.PtrToStringAnsi(Path()));
+        Debug.Log(Marshal.PtrToStringAnsi(GetStatus()));
+        Debug.Log(Hatches());
     }
 }
